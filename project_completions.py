84f0255a0e309a1f09cda40ef2783ff389e8cc2c
Vasiliy Sorokin <vasiliy.sorokin@opensoftdev.ru>
2016-04-21 23:09:30 +0300

    Add more new logic

--- project_completions.py
+++ project_completions.py
@@ -5,7 +5,9 @@ import sublime_plugin
 import sublime
 import re
 import time
-from os.path import basename
+import os
+import os.path
+import json
 
 # limits to prevent bogging down the system
 MIN_WORD_SIZE = 3
@@ -23,31 +25,50 @@ class ProjectAutocomplete(sublime_plugin.EventListener):
 
         # Limit number of files but always include the active opened file. This
         # view goes first to prioritize matches close to cursor position.
-        words = get_words_from_view(view, locations)
-        words.extend(get_words_from_files(prefix))
+        words = get_words_from_view(prefix, view, locations)
+        words.extend(get_words_from_files(prefix, view.window))
 
         words = without_duplicates(words)
         matches = []
-        for w, v in words:
-            trigger = w
-            contents = w.replace('$', '\\$')
-            if v.id != view.id and v.file_name():
-                trigger += '\t(%s)' % basename(v.file_name())
+        for word, filename in words:
+            trigger = word
+            contents = word.replace('$', '\\$')
+            if len(filename) > 0:
+                trigger += '\t(%s)' % os.path.basename(filename)
             matches.append((trigger, contents))
         return matches
 
-def get_words_from_view(view, locations):
+
+def get_words_from_view(prefix, view, locations):
     words = []
     if len(locations) > 0:
-        view_words = v.extract_completions(prefix, locations[0])
-        view_words = filter_words(view_words)
-        view_words = fix_truncation(v, view_words)
-        words += [(w, v) for w in view_words]
+        words = view.extract_completions(prefix, locations[0])
+    else:
+        words = view.extract_completions(prefix)
+    words = filter_words(words)
+    words = fix_truncation(view, words)
+    return [(word, "") for word in words]
 
-def get_words_from_files(prefix):
+
+def get_words_from_files(prefix, window):
     words = []
-    #TODO: get words from files here
+    project_data = json.loads(window.project_data())
+    folders = project_data["folders"]
+    for folder in folders:
+        path = folder["path"]
+        files = os.listdir(path)
+        for filename in files:
+            file_words = get_words_from_file(filename, prefix)
+            file_words = filter_words(file_words)
+            words += [(word, filename) for word in file_words]
+
     return words
+
+
+def get_words_from_file(filename, prefix):
+        # TODO: get words from files here
+        pass
+
 
 def filter_words(words):
     words = words[0:MAX_WORDS_PER_FILE]
@@ -73,14 +94,14 @@ def fix_truncation(view, words):
     start_time = time.time()
 
     for i, w in enumerate(words):
-        #The word is truncated if and only if it cannot be found with a word boundary before and after
+        # The word is truncated if and only if it cannot be found with a word boundary before and after
 
         # this fails to match strings with trailing non-alpha chars, like
         # 'foo?' or 'bar!', which are common for instance in Ruby.
         match = view.find(r'\b' + re.escape(w) + r'\b', 0)
         truncated = is_empty_match(match)
         if truncated:
-            #Truncation is always by a single character, so we extend the word by one word character before a word boundary
+            # Truncation is always by a single character, so we extend the word by one word character before a word boundary
             extended_words = []
             view.find_all(r'\b' + re.escape(w) + r'\w\b', 0, "$0", extended_words)
             if len(extended_words) > 0:
@@ -90,7 +111,7 @@ def fix_truncation(view, words):
                 # use the old word if we didn't find any extended matches
                 fixed_words.append(w)
         else:
-            #Pass through non-truncated words
+            # Pass through non-truncated words
             fixed_words.append(w)
 
         # if too much time is spent in here, bail out,
